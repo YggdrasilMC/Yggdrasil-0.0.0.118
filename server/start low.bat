3dba79611a3602868272e1108b4fbcf7ac4e0307
Yggdrasil <help@yggdrasilmc.com>
2022-06-18 20:52:37 +1200

    Server 1

---
+++ server/start low.bat
@@ -0,0 +1,4 @@
+@echo off
+title Yggdrasil 1.18.2
+java -Xms2G -Xmx4G -jar fabric-server-launch.jar -nogui
+pause
---
+++ server/start.bat
@@ -0,0 +1,4 @@
+@echo off
+title Yggdrasil 1.18.2
+java.exe -Xms8G -Xmx8G -jar fabric-server-launch.jar -nogui
+pause
